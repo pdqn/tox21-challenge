2dae29d621a3ad6f4f5b18432e323b902caad845
pdcoded <prabhudatta.das09@gmail.com>
2017-11-19 01:23:04 +0530

    imputed na with zero since loss function was throwing up nan

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+
 import skchem
 import pandas as pd
 from scipy import io
@@ -43,16 +44,9 @@ X, y = pipeline.transform_filter(ms_raw)
 
 print X.shape
 
-The Tox21 data set comprises 12,060 training samples 
-and 647 test samples that represent chemical compounds. 
-There are 801 "dense features" that represent chemical descriptors, 
-such as molecular weight, solubility or surface area, and 272,776 "sparse features" 
-that represent chemical substructures (ECFP10, DFS6, DFS8; stored in 
-Matrix Market Format ). Machine learning methods can either use sparse or 
-dense data or combine them. 
-For each sample there are 12 binary labels that represent 
-the outcome (active/inactive) of 12 different toxicological experiments. 
-Note that the label matrix contains many missing values (NAs). 
-The original data source and Tox21 challenge site is https://tripod.nih.gov/tox21/challenge/.
-
+There are various types of input features 
+such as physico-chemical descriptors, 
+extended-connectivity fingerprints (ECFP) 
+and other molecular descriptors that are typically used in that research areas.
+Use rdkit and skchem python libraries
 """
